3f3e98a37d9bb6610fc8e4bb4581031ee79e02c3
Rika16 <69685209+Rika16@users.noreply.github.com>
2022-04-07 19:33:18 +0300

    Pc version

---
+++ js/swiper-reviews.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+   slidesPerView: 3,
+   spaceBetween: 30,
+   slidesPerGroup: 1,
+   loop: true,
+   loopFillGroupWithBlank: true,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   
+   responsive: [{
+      breakpoint: 768,
+      settings: {
+          arrows : false
+      }
+  }, ]
+});
+       
