3e05acd1250edfe5a1eb1fe4d75ae421d3e12470
Rika16 <69685209+Rika16@users.noreply.github.com>
2022-04-08 17:25:16 +0300

    mob-version

--- js/swiper-reviews.js
+++ js/swiper-reviews.js
@@ -1,23 +1,30 @@
-var swiper = new Swiper(".mySwiper", {
-   slidesPerView: 3,
-   spaceBetween: 30,
+const swiper = new Swiper('.swiper', {
+  // Default parameters
+   slidesPerView: 1,
+   spaceBetween: 10,
    slidesPerGroup: 1,
+   freeMode: true,
+
    loop: true,
    loopFillGroupWithBlank: true,
    pagination: {
       el: ".swiper-pagination",
       clickable: true,
    },
-   navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-   },
    
-   responsive: [{
-      breakpoint: 768,
-      settings: {
-          arrows : false
+   breakpoints: {
+      768:{
+         slidesPerView: 2,
+         spaceBetween: 10,
+      },
+
+      1024:{
+         slidesPerView: 3,
+         spaceBetween: 10,
       }
-  }, ]
+         
+
+      }
 });
-       
+
+  
